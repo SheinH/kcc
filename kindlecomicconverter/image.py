67b7d71690c045dc655d8c32d36d6cff6c04b183
SheinH <chocolateshein.htike@gmail.com>
2024-11-08 19:23:13 -0500

    Made everything use .contain()

--- kindlecomicconverter/image.py
+++ kindlecomicconverter/image.py
@@ -361,14 +361,14 @@ class ComicPage:
             borderh = int((self.size[1] - self.image.size[1]) / 2)
             self.image = ImageOps.expand(self.image, border=(borderw, borderh), fill=self.fill)
             if self.image.size[0] != self.size[0] or self.image.size[1] != self.size[1]:
-                self.image = ImageOps.fit(self.image, self.size, method=method)
+                self.image = ImageOps.contain(self.image, self.size, method=method)
         else:  # if image bigger than device resolution or smaller with upscaling
             if self.rotated or self.under_crop_minimum:
-                self.image = ImageOps.pad(self.image, self.size, method=method, color="white")
+                self.image = ImageOps.contain(self.image, self.size, method=method)
             elif abs(ratio_image - ratio_device) < AUTO_CROP_THRESHOLD:
-                self.image = ImageOps.cover(self.image, self.size, method=method)
+                self.image = ImageOps.contain(self.image, self.size, method=method)
             elif self.opt.format == 'CBZ' or self.opt.kfx:
-                self.image = ImageOps.cover(self.image, self.size, method=method)
+                self.image = ImageOps.contain(self.image, self.size, method=method)
             else:
                 print("notmy fix4!!")
                 if self.kindle_scribe_azw3:
